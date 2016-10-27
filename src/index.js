82ef77ffea3a94eb4859385b3283d975406550b6
Luca Pillonel <lpi@imtf.ch>
2016-10-27 15:42:22 +0200

    - pure functions as methods class to optimize parentScroll finding - lazyload on resize also for overflow elements - remove forceCheck export - updated package - version 2.2.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
- * react-lazyload
- */
+* react-lazyload
+*/
 import React, { Component, PropTypes } from 'react';
 import ReactDom from 'react-dom';
 import { on, off } from './utils/event';
@@ -13,260 +13,246 @@ const listeners = [];
 let pending = [];
 
 
-/**
- * Check if `component` is visible in overflow container `parent`
- * @param  {node} component React component
- * @param  {node} parent    component's scroll parent
- * @return {bool}
- */
-const checkOverflowVisible = function checkOverflowVisible(component, parent) {
-  const node = ReactDom.findDOMNode(component);
-
-  const { top: parentTop, height: parentHeight } = parent.getBoundingClientRect();
-  const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight;
-
-  // calculate top and height of the intersection of the element's scrollParent and viewport
-  const intersectionTop = Math.max(parentTop, 0); // intersection's top relative to viewport
-  const intersectionHeight = Math.min(windowInnerHeight, parentTop + parentHeight) - intersectionTop; // height
-
-  // check whether the element is visible in the intersection
-  const { top, height } = node.getBoundingClientRect();
-  const offsetTop = top - intersectionTop; // element's top relative to intersection
-
-  const offsets = Array.isArray(component.props.offset) ?
-                component.props.offset :
-                [component.props.offset, component.props.offset]; // Be compatible with previous API
-
-  return (offsetTop - offsets[0] <= intersectionHeight) &&
-         (offsetTop + height + offsets[1] >= 0);
-};
-
-/**
- * Check if `component` is visible in document
- * @param  {node} component React component
- * @return {bool}
- */
-const checkNormalVisible = function checkNormalVisible(component) {
-  const node = ReactDom.findDOMNode(component);
-
-  const { top, height: elementHeight } = node.getBoundingClientRect();
-
-  const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight;
-
-  const offsets = Array.isArray(component.props.offset) ?
-                component.props.offset :
-                [component.props.offset, component.props.offset]; // Be compatible with previous API
-
-  return (top - offsets[0] <= windowInnerHeight) &&
-         (top + elementHeight + offsets[1] >= 0);
-};
-
-
-/**
- * Detect if element is visible in viewport, if so, set `visible` state to true.
- * If `once` prop is provided true, remove component as listener after checkVisible
- *
- * @param  {React} component   React component that respond to scroll and resize
- */
-const checkVisible = function checkVisible(component) {
-  const node = ReactDom.findDOMNode(component);
-  if (!node) {
-    return;
-  }
-
-  const parent = scrollParent(node);
-  const isOverflow = parent !== node.ownerDocument &&
-                     parent !== document &&
-                     parent !== document.documentElement;
-
-  const visible = isOverflow ?
-                  checkOverflowVisible(component, parent) :
-                  checkNormalVisible(component);
-
-  if (visible) {
-    // Avoid extra render if previously is visible, yeah I mean `render` call,
-    // not actual DOM render
-    if (!component.visible) {
-      if (component.props.once) {
-        pending.push(component);
-      }
-
-      component.visible = true;
-      component.forceUpdate();
-    }
-  } else if (!(component.props.once && component.visible)) {
-    component.visible = false;
-  }
-};
-
-
-const purgePending = function purgePending() {
-  pending.forEach(component => {
-    const index = listeners.indexOf(component);
-    if (index !== -1) {
-      listeners.splice(index, 1);
-    }
-  });
-
-  pending = [];
-};
-
-
-const lazyLoadHandler = () => {
-  for (let i = 0; i < listeners.length; ++i) {
-    const listener = listeners[i];
-    checkVisible(listener);
-  }
-
-  // Remove `once` component in listeners
-  purgePending();
-};
-
 // Depending on component's props
 let delayType;
 let finalLazyLoadHandler = null;
 
 
 class LazyLoad extends Component {
-  constructor(props) {
-    super(props);
 
-    this.visible = false;
+    constructor(props) {
+        super(props);
 
-  }
+        this.parent;
+        this.visible = false;
 
-  componentDidMount() {
-    if (typeof process !== 'undefined' && process.env.NODE_ENV !== 'production') {
-      if (React.Children.count(this.props.children) > 1) {
-        console.warn('[react-lazyload] Only one child is allowed to be passed to `LazyLoad`.');
-      }
-
-      if (this.props.wheel) { // eslint-disable-line
-        console.warn('[react-lazyload] Props `wheel` is not supported anymore, try set `overflow` for lazy loading in overflow containers.');
-      }
-
-      // Warn the user if placeholder and height is not specified and the rendered height is 0
-      if (!this.props.placeholder && this.props.height === undefined && ReactDom.findDOMNode(this).offsetHeight === 0) {
-        console.warn('[react-lazyload] Please add `height` props to <LazyLoad> for better performance.');
-      }
     }
 
-    // It's unlikely to change delay type on the fly, this is mainly
-    // designed for tests
-    let needResetFinalLazyLoadHandler = false;
-    if (this.props.debounce !== undefined && delayType === 'throttle') {
-      console.warn('[react-lazyload] Previous delay function is `throttle`, now switching to `debounce`, try setting them unanimously');
-      needResetFinalLazyLoadHandler = true;
-    } else if (delayType === 'debounce' && this.props.debounce === undefined) {
-      console.warn('[react-lazyload] Previous delay function is `debounce`, now switching to `throttle`, try setting them unanimously');
-      needResetFinalLazyLoadHandler = true;
-    }
+    componentDidMount() {
+        this.parent = scrollParent(ReactDom.findDOMNode(this));
 
-    if (needResetFinalLazyLoadHandler) {
-      off(window, 'scroll', finalLazyLoadHandler);
-      off(window, 'resize', finalLazyLoadHandler);
-      finalLazyLoadHandler = null;
-    }
-
-    if (!finalLazyLoadHandler) {
-      if (this.props.debounce !== undefined) {
-        finalLazyLoadHandler = debounce(lazyLoadHandler, typeof this.props.debounce === 'number' ?
-                                                         this.props.debounce :
-                                                         300);
-        delayType = 'debounce';
-      } else {
-        finalLazyLoadHandler = throttle(lazyLoadHandler, typeof this.props.throttle === 'number' ?
-                                                         this.props.throttle :
-                                                         300);
-        delayType = 'throttle';
-      }
-    }
-
-    if (this.props.overflow) {
-      const parent = scrollParent(ReactDom.findDOMNode(this));
-      if (parent) {
-        const listenerCount = 1 + (+parent.getAttribute(LISTEN_FLAG));
-        if (listenerCount === 1) {
-          parent.addEventListener('scroll', finalLazyLoadHandler);
+        // It's unlikely to change delay type on the fly, this is mainly
+        // designed for tests
+        let needResetFinalLazyLoadHandler = false;
+        if (this.props.debounce !== undefined && delayType === 'throttle') {
+            console.warn('[react-lazyload] Previous delay function is `throttle`, now switching to `debounce`, try setting them unanimously');
+            needResetFinalLazyLoadHandler = true;
+        } else if (delayType === 'debounce' && this.props.debounce === undefined) {
+            console.warn('[react-lazyload] Previous delay function is `debounce`, now switching to `throttle`, try setting them unanimously');
+            needResetFinalLazyLoadHandler = true;
         }
-        parent.setAttribute(LISTEN_FLAG, listenerCount);
-      }
-    } else if (listeners.length === 0 || needResetFinalLazyLoadHandler) {
-      const { scroll, resize } = this.props;
 
-      if (scroll) {
-        on(window, 'scroll', finalLazyLoadHandler);
-      }
-
-      if (resize) {
-        on(window, 'resize', finalLazyLoadHandler);
-      }
-    }
-
-    listeners.push(this);
-    checkVisible(this);
-  }
-
-  shouldComponentUpdate() {
-    return this.visible;
-  }
-
-  componentWillUnmount() {
-    if (this.props.overflow) {
-      const parent = scrollParent(ReactDom.findDOMNode(this));
-      if (parent) {
-        const listenerCount = (+parent.getAttribute(LISTEN_FLAG)) - 1;
-        if (listenerCount === 0) {
-          parent.removeEventListener('scroll', finalLazyLoadHandler);
-          parent.removeAttribute(LISTEN_FLAG);
-        } else {
-          parent.setAttribute(LISTEN_FLAG, listenerCount);
+        if (needResetFinalLazyLoadHandler) {
+            off(window, 'scroll', finalLazyLoadHandler);
+            off(window, 'resize', finalLazyLoadHandler);
+            finalLazyLoadHandler = null;
         }
-      }
+
+        if (!finalLazyLoadHandler) {
+            if (this.props.debounce !== undefined) {
+                finalLazyLoadHandler = debounce(this.lazyLoadHandler.bind(this), typeof this.props.debounce === 'number' ?
+                this.props.debounce :
+                300);
+                delayType = 'debounce';
+            } else {
+                finalLazyLoadHandler = throttle(this.lazyLoadHandler.bind(this), typeof this.props.throttle === 'number' ?
+                this.props.throttle :
+                300);
+                delayType = 'throttle';
+            }
+        }
+
+        if (this.props.overflow) {
+            if (this.parent) {
+                const listenerCount = 1 + (+this.parent.getAttribute(LISTEN_FLAG));
+                if (listenerCount === 1) {
+                    this.parent.addEventListener('scroll', finalLazyLoadHandler);
+
+                    if (this.props.resize) {
+                        on(window, 'resize', finalLazyLoadHandler);
+                    }
+                }
+                this.parent.setAttribute(LISTEN_FLAG, listenerCount);
+            }
+        } else if (listeners.length === 0 || needResetFinalLazyLoadHandler) {
+            const { scroll, resize } = this.props;
+
+            if (scroll) {
+                on(window, 'scroll', finalLazyLoadHandler);
+            }
+
+            if (resize) {
+                on(window, 'resize', finalLazyLoadHandler);
+            }
+        }
+
+        listeners.push(this);
+        this.checkVisible(this);
     }
 
-    const index = listeners.indexOf(this);
-    if (index !== -1) {
-      listeners.splice(index, 1);
+    shouldComponentUpdate() {
+        return this.visible;
     }
 
-    if (listeners.length === 0) {
-      off(window, 'resize', finalLazyLoadHandler);
-      off(window, 'scroll', finalLazyLoadHandler);
-    }
-  }
+    componentWillUnmount() {
+        if (this.props.overflow) {
+            if (this.parent) {
+                const listenerCount = (+this.parent.getAttribute(LISTEN_FLAG)) - 1;
+                if (listenerCount === 0) {
+                    this.parent.removeEventListener('scroll', finalLazyLoadHandler);
+                    this.parent.removeAttribute(LISTEN_FLAG);
+                } else {
+                    this.parent.setAttribute(LISTEN_FLAG, listenerCount);
+                }
+            }
+        }
 
-  render() {
-    return this.visible ?
-           this.props.children :
-             this.props.placeholder ?
-                this.props.placeholder :
-                <div style={{ height: this.props.height }} className="lazyload-placeholder"></div>;
-  }
+        const index = listeners.indexOf(this);
+        if (index !== -1) {
+            listeners.splice(index, 1);
+        }
+
+        if (listeners.length === 0) {
+            off(window, 'resize', finalLazyLoadHandler);
+            off(window, 'scroll', finalLazyLoadHandler);
+        }
+    }
+
+    /**
+    * Check if `component` is visible in overflow container `parent`
+    * @param  {node} component React component
+    * @param  {node} parent    component's scroll parent
+    * @return {bool}
+    */
+    checkOverflowVisible(component, parent) {
+        const node = ReactDom.findDOMNode(component);
+
+        const { top: parentTop, height: parentHeight } = parent.getBoundingClientRect();
+        const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight;
+
+        // calculate top and height of the intersection of the element's scrollParent and viewport
+        const intersectionTop = Math.max(parentTop, 0); // intersection's top relative to viewport
+        const intersectionHeight = Math.min(windowInnerHeight, parentTop + parentHeight) - intersectionTop; // height
+
+        // check whether the element is visible in the intersection
+        const { top, height } = node.getBoundingClientRect();
+        const offsetTop = top - intersectionTop; // element's top relative to intersection
+
+        const offsets = Array.isArray(component.props.offset) ?
+        component.props.offset :
+        [component.props.offset, component.props.offset]; // Be compatible with previous API
+
+        return (offsetTop - offsets[0] <= intersectionHeight) &&
+        (offsetTop + height + offsets[1] >= 0);
+    }
+
+    /**
+    * Check if `component` is visible in document
+    * @param  {node} component React component
+    * @return {bool}
+    */
+    checkNormalVisible(component) {
+        const node = ReactDom.findDOMNode(component);
+
+        const { top, height: elementHeight } = node.getBoundingClientRect();
+
+        const windowInnerHeight = window.innerHeight || document.documentElement.clientHeight;
+
+        const offsets = Array.isArray(component.props.offset) ?
+        component.props.offset :
+        [component.props.offset, component.props.offset]; // Be compatible with previous API
+
+        return (top - offsets[0] <= windowInnerHeight) &&
+        (top + elementHeight + offsets[1] >= 0);
+    }
+
+    /**
+    * Detect if element is visible in viewport, if so, set `visible` state to true.
+    * If `once` prop is provided true, remove component as listener after checkVisible
+    *
+    * @param  {React} component   React component that respond to scroll and resize
+    */
+    checkVisible(component) {
+        const node = ReactDom.findDOMNode(component);
+        if (!node) {
+            return;
+        }
+
+        const isOverflow = this.parent !== node.ownerDocument &&
+        this.parent !== document &&
+        this.parent !== document.documentElement;
+
+        const visible = isOverflow ?
+        this.checkOverflowVisible(component, this.parent) :
+        this.checkNormalVisible(component);
+
+        if (visible) {
+            // Avoid extra render if previously is visible, yeah I mean `render` call,
+            // not actual DOM render
+            if (!component.visible) {
+                if (component.props.once) {
+                    pending.push(component);
+                }
+
+                component.visible = true;
+                component.forceUpdate();
+            }
+        } else if (!(component.props.once && component.visible)) {
+            component.visible = false;
+        }
+    }
+
+    purgePending() {
+        pending.forEach(component => {
+            const index = listeners.indexOf(component);
+            if (index !== -1) {
+                listeners.splice(index, 1);
+            }
+        });
+
+        pending = [];
+    }
+
+    lazyLoadHandler() {
+        for (let i = 0; i < listeners.length; ++i) {
+            const listener = listeners[i];
+            this.checkVisible(listener);
+        }
+
+        // Remove `once` component in listeners
+        this.purgePending();
+    };
+
+    render() {
+        return this.visible ?
+        this.props.children :
+        this.props.placeholder ?
+        this.props.placeholder :
+        <div style={{ height: this.props.height }} className="lazyload-placeholder"></div>;
+    }
 }
 
 LazyLoad.propTypes = {
-  once: PropTypes.bool,
-  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  offset: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
-  overflow: PropTypes.bool,
-  resize: PropTypes.bool,
-  scroll: PropTypes.bool,
-  children: PropTypes.node,
-  throttle: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
-  debounce: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
-  placeholder: PropTypes.node
+    once: PropTypes.bool,
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    offset: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
+    overflow: PropTypes.bool,
+    resize: PropTypes.bool,
+    scroll: PropTypes.bool,
+    children: PropTypes.node,
+    throttle: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+    debounce: PropTypes.oneOfType([PropTypes.number, PropTypes.bool]),
+    placeholder: PropTypes.node
 };
 
 LazyLoad.defaultProps = {
-  once: false,
-  offset: 0,
-  overflow: false,
-  resize: false,
-  scroll: true
+    once: false,
+    offset: 0,
+    overflow: false,
+    resize: false,
+    scroll: true
 };
 
 import decorator from './decorator';
 export const lazyload = decorator;
 export default LazyLoad;
-export { lazyLoadHandler as forceCheck };
